9c85634d96ded1996875b5525fdcc4495131bd42
Erick <erick.rholanda@gmail.com>
2019-10-28 07:50:51 -0300

    Corrigindo disposição dos cards no responsivo

--- app/src/components/CardUserList/index.js
+++ app/src/components/CardUserList/index.js
@@ -6,7 +6,7 @@ export default function CardUserList(props) {
     const { users } = props;
 
     return ( <Grid container spacing={3}>
-        {users.map( (user, key) =><Grid item xs={3} md={3} lg={4} sm={6} key={key}>
+        {users.map( (user, key) =><Grid item xs={12} md={4} lg={3} sm={6} key={key}>
             <CardUser user={user} />
         </Grid>)}
    </Grid>);
